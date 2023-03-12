21a2fa5579650bfb61a46e06dcb68d79a3aa4149
Игнат Лукьянчук <115699352+PhoenixGoIt@users.noreply.github.com>
2023-03-12 12:11:02 +0200

    t

--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,40 @@
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
 
+
+player.on('timeupdate', (function(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  })); 
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime)
+}
+
+//------------------------------------------------Ошибки------------------------------------------------\\
+//const Vimeo = require('@vimeo/player');
+//window.Vimeo = require('@vimeo/player');
+//import Vimeo from '@vimeo/player'; 
+
+//Uncaught TypeError: (0 , _playerDefault.default).Player is not a constructor
+//at 2KCbX.@parcel/transformer-js/src/esmodule-helpers.js (02-video.cc7354fd.js:564:16)
+//at newRequire (02-video.cc7354fd.js:71:24)
+//at 02-video.cc7354fd.js:122:5
+//at 02-video.cc7354fd.js:145:3
+
+
+
+//Выдаёт эту ошибку, пытался исправить но ничего не помогло :\
+
+
+
+//player.on('timeupdate', _.throttle(function(data) {
+//    localStorage.setItem('videoplayer-current-time', data.seconds);
+//  }, 1000));
+
+//Uncaught ReferenceError: _ is not defined
+//at 2KCbX (02-video.cc7354fd.js:562:25)
+//at newRequire (02-video.cc7354fd.js:71:24)
+//at 02-video.cc7354fd.js:122:5
+//at 02-video.cc7354fd.js:145:3
+//------------------------------------------------Ошибки------------------------------------------------\\
--- js/03-feedback.js
+++ js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const EmailInput = form.querySelector('input');
+const MessageTextarea = form.querySelector('textarea');
+
+fillForm();
+
+form.addEventListener('submit', FormSubmit);
+form.addEventListener('input', throttle(FormInput, 500));
+
+function FormSubmit(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.target.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+function FormInput(e) {
+  const formData = {
+    email: EmailInput.value,
+    message: MessageTextarea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+function fillForm() {
+  const saveData = localStorage.getItem('feedback-form-state');
+  if (saveData) {
+    const parsedFormData = JSON.parse(saveData);
+    EmailInput.value = parsedFormData.email;
+    MessageTextarea.value = parsedFormData.message;
+  }
+}
